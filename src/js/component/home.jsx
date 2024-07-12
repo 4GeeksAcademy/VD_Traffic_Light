c8886a1b22de03fba74fa5eef934d90f04f2e491
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-07-12 03:18:04 +0000

    proyecto finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
+	// declaro los estados para los botones
+	let [red, setRed]=useState('border-danger');
+	let [ green, setGreen]=useState('border-success');
+	let [ yellow, setYellow]=useState('border-warning');
+	
+	// genero la funcion que va a determinar el estado para cada boton
+	function state_Click(e){
+		
+		if(e.target.id==="red"){
+			setRed('border-primary')
+			setGreen('bg opacity-50 border-success')
+			setYellow('bg opacity-50 border-warning')
+		}else if(e.target.id==="yellow"){
+			setYellow('border-primary')
+			setGreen('bg opacity-50 border-success')
+			setRed('bg opacity-50 border-warning')
+		}else{
+			setGreen('border-primary')
+			setYellow('bg opacity-50 border-success')
+			setRed('bg opacity-50 border-warning')
+		}
+
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		// genero los contenedores para el fondo y ubicacion de los botones
+		<div className="d-flex align-items-center justify-content-center vh-100">
+		<div className="bg-dark p-5 d-flex flex-column justify-content-center align-items-center gap-2 rounded">			
+			{/* al evento onClick del componente button le paso la funcion que va a determinar el estado de los botones */}
+			<button onClick={state_Click} id="red" className={"p-5 rounded rounded-pill bg-danger "+ red} style={{width: '50px'}}></button>
+			<button onClick={state_Click} id="yellow" className={"p-5 rounded rounded-pill bg-warning "+ yellow} style={{width: '50px'}}></button>
+			<button onClick={state_Click} id="green" className={"p-5 rounded rounded-pill bg-success "+ green} style={{width: '50px'}}></button>
+		</div>
 		</div>
 	);
 };
