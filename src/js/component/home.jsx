f399687b13d4be00b20241e96f2790a4d97a3180
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-07-17 18:55:02 +0000

    refactorizando proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,27 +6,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	// declaro los estados para los botones
-	let [red, setRed]=useState('border-danger');
-	let [ green, setGreen]=useState('border-success');
-	let [ yellow, setYellow]=useState('border-warning');
+	// declaro el estado del boton
+	   let   [chosenColor, setChosenColor]=useState("id");
 	
-	// genero la funcion que va a determinar el estado para cada boton
-	function state_Click(e){
-		
-		if(e.target.id==="red"){
-			setRed('border-primary')
-			setGreen('bg opacity-50 border-success')
-			setYellow('bg opacity-50 border-warning')
-		}else if(e.target.id==="yellow"){
-			setYellow('border-primary')
-			setGreen('bg opacity-50 border-success')
-			setRed('bg opacity-50 border-warning')
-		}else{
-			setGreen('border-primary')
-			setYellow('bg opacity-50 border-success')
-			setRed('bg opacity-50 border-warning')
-		}
+	// genero la funcion que va a determinar a que boton le hago click
+	function selectedColor(e){
+		setChosenColor(e.target.id);
 
 	}
 	return (
@@ -34,9 +19,9 @@ const Home = () => {
 		<div className="d-flex align-items-center justify-content-center vh-100">
 		<div className="bg-dark p-5 d-flex flex-column justify-content-center align-items-center gap-2 rounded">			
 			{/* al evento onClick del componente button le paso la funcion que va a determinar el estado de los botones */}
-			<button onClick={state_Click} id="red" className={"p-5 rounded rounded-pill bg-danger "+ red} style={{width: '50px'}}></button>
-			<button onClick={state_Click} id="yellow" className={"p-5 rounded rounded-pill bg-warning "+ yellow} style={{width: '50px'}}></button>
-			<button onClick={state_Click} id="green" className={"p-5 rounded rounded-pill bg-success "+ green} style={{width: '50px'}}></button>
+			<button onClick={selectedColor} id="red" className={`p-5 rounded rounded-pill bg-danger ${chosenColor=="red" ? "opacity-100":"opacity-50"}`} style={{width: '50px'}}></button>
+			<button onClick={selectedColor} id="yellow" className={`p-5 rounded rounded-pill bg-warning ${chosenColor=="yellow" ? "opacity-100":"opacity-50"}`} style={{width: '50px'}}></button>
+			<button onClick={selectedColor} id="green" className={`p-5 rounded rounded-pill bg-success ${chosenColor=="green" ? "opacity-100":"opacity-50"}`} style={{width: '50px'}}></button>
 		</div>
 		</div>
 	);
